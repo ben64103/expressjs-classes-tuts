28d93d9ef1cfa8cd7600aee53a0cf231ed1a3cb8
ben64103 <benabolade@gmail.com>
2023-11-09 12:25:28 +0100

    tuts: Auth routes & controllers

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,4 @@
-// const { Router } = require('express');
-// const router = Router();
-
-const router = require('express').Router();
+const router = require('express').Router()
 
 const authController = require('../controllers/authController');
 
